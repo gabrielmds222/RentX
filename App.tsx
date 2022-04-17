af2ed4199c209faa7c1c62e64fe4cf1dce710ad3
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-04-17 12:54:15 -0300

    feat: Adiciona cores e fontes globais

--- App.tsx
+++ App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
 
 import { Home } from './src/screens/home'; 
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  });
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <Home />
   );
