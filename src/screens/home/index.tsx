92f2b4703a4035ecd0d1a2edadcc9dd3eb964616
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-04-19 00:52:19 -0300

    feat: Finaliza estilização do header

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { RFValue} from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
 
 import {
     Container,
     Header,
+    HeaderContent,
+    TotalCars,
 } from './styles';
 
 export function Home(){
@@ -18,7 +21,17 @@ export function Home(){
       />
 
       <Header>
-        <Logo />
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+        
       </Header>
     </Container>
   );
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
     flex: 1;
@@ -8,5 +9,21 @@ export const Container = styled.View`
 export const Header = styled.View`
     width: 100%;
     height: 113px;
+
     background-color: ${({ theme }) => theme.colors.header};
+    
+    justify-content: flex-end;
+    padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+    font-size: ${RFValue(15)}px;
+    font-family: ${({ theme }) => theme.fonts.primary_400};
+    color: ${({ theme }) => theme.colors.text};
 `;
