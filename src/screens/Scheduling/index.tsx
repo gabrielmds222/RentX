cf7f0e6b7a1c6df6929e135460595918d84c9aff
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-05-21 23:31:29 -0300

    feat: Finaliza header

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
 
 import {
     Container,
+    Header,
+    Title,
 } from './styles';
 
 export function Scheduling(){
+  const theme = useTheme();
+
   return(
     <Container>
+      <Header>
+        <BackButton 
+        onPress={() => {}}
+        color={theme.colors.shape}
+        />
 
+        <Title>
+           Escolha uma {'\n'} data de inicio e {'\n'} fim do aluguel
+        </Title>
+      </Header>
     </Container>
   );
 }
--- src/screens/Scheduling/styles.ts
+++ src/screens/Scheduling/styles.ts
@@ -1,5 +1,24 @@
-import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+`;
 
+export const Header = styled.View`
+  width: 100%;
+  height: 325px;
+  background-color: ${({ theme }) => theme.colors.header};
+  justify-content: center;
+  padding: 24px;
+  padding-top: ${getStatusBarHeight() + 30}px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  font-size: ${RFValue(34)}px;
+  margin-top: 24px;
 `;
