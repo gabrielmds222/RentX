49edb1671aea8f4d7a428387ae04ff7de38bda84
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-05-22 14:06:59 -0300

    feat: Customiza calendário

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
 
-import { Calendar as CustomCalendar } from 'react-native-calendars';
+import { Calendar as CustomCalendar, LocaleConfig } from 'react-native-calendars';
 
-import {
-    Container,
-} from './styles';
+LocaleConfig.locales['pt-br'] = {
+  monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+  monthNamesShort: ['Jan', 'Feb', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dec'],
+  dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  dayNamesShort: ['DOM', 'SEG', 'TER,', 'QUA', 'QUI', 'SEX', 'SÁB'],
+  // today: 'Hoje',
+}
+LocaleConfig.defaultLocale = 'pt-br'
 
 export function Calendar(){
-  return(
-    <CustomCalendar>
+  const theme = useTheme();
 
-    </CustomCalendar>
+  return(
+    <CustomCalendar 
+      renderArrow={(direction) => 
+        <Feather
+          size={24}
+          color={theme.colors.text}
+          name={direction === 'left' ? 'chevron-left' : 'chevron-right'}
+        />
+      }
+      headerStyle={{
+        backgroundColor: theme.colors.background_secondary,
+        borderBottomWidth: 0.5,
+        borderBottomColor: theme.colors.text_detail,
+        paddingBottom: 10,
+        marginBottom: 10,
+      }}
+      theme={{
+        textDayFontFamily: theme.fonts.primary_400,
+        textDayHeaderFontFamily: theme.fonts.primary_500,
+        textDayHeaderFontSize: 10,
+        textMonthFontFamily: theme.fonts.secondary_600,
+        textMonthFontSize: 20,
+        monthTextColor: theme.colors.title,
+        arrowStyle: {
+          marginHorizontal: -15,
+        },
+      }}
+      firstDay={1}
+      minDate={new Date()}
+    />
   );
 }
