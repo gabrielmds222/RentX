7e33dc7ddeb4c7163336410d03e7e4e04266674c
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-05-24 23:53:10 -0300

    feat: Estiliza tela de confirmação

--- src/screens/SchedulingComplete/styles.ts
+++ src/screens/SchedulingComplete/styles.ts
@@ -1,5 +1,38 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
 
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.header};
+  padding-top: 96px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 80px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(30)}px;
+  margin-top: 40px;
+`;
+
+export const Message = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text_detail};
+  font-size: ${RFValue(15)}px;
+  line-height: ${RFValue(25)}px;
+  text-align: center;
+  margin-top: 30px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  align-items: center;
+  margin: 0 0 40px;
 `;
