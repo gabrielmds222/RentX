f2b4a31a7b5eb1317cabbb46965d8183d40f16c0
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-06-10 03:59:38 -0300

    feat: Cria menu flutuante

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -3,6 +3,8 @@ import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { RFValue} from 'react-native-responsive-fontsize';
 
+import { Ionicons } from '@expo/vector-icons';
+
 import { Car } from '../../components/Car';
 import { Load } from '../../components/Load';
 
@@ -10,21 +12,29 @@ import Logo from '../../assets/logo.svg';
 import api from '../../services/api';
 import { CarDTO } from '../../dtos/carDTO';
 
+import { useTheme } from 'styled-components';
+
 import {
     Container,
     Header,
     HeaderContent,
     TotalCars,
     CarList,
+    MyCarsButton,
 } from './styles';
 
 export function Home(){
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
+  const theme = useTheme();
 
   function handleCarDetails(car: CarDTO) {
-    navigation.navigate('CarDetails', { car })
+    navigation.navigate('CarDetails', { car });
+  }
+  
+  function handleOpenMyCars() {
+    navigation.navigate('MyCars');
   }
 
   useEffect(() => {
@@ -71,6 +81,14 @@ export function Home(){
           <Car data={item} onPress={() => handleCarDetails(item)}/>}
         />
       }
+
+      <MyCarsButton onPress={handleOpenMyCars}>
+        <Ionicons 
+        name="ios-car-sport" 
+        size={32}
+        color={theme.colors.shape}
+        />
+      </MyCarsButton>
     </Container>
   );
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { FlatList } from 'react-native';
 import { CarDTO } from '../../dtos/carDTO';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
     flex: 1;
@@ -36,3 +37,18 @@ export const CarList = styled(FlatList as new () => FlatList<CarDTO>).attrs({
       padding: 24,
     }
   })``;
+
+export const MyCarsButton = styled(RectButton)`
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+
+    justify-content: center;
+    align-items: center;
+
+    background-color: ${({ theme }) => theme.colors.main};
+
+    position: absolute;
+    bottom: 13px;
+    right: 22px;
+`;
