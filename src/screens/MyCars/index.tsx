77a1af25731b8b3c6a68efaa4bb16a66595492e2
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-06-10 04:17:43 -0300

    feat: Adiciona o get para pegar os carros vinculados ao usuário

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
-
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import { Car } from '../../components/Car';
+import { CarDTO } from '../../dtos/CarDTO';
+import  api  from '../../services/api';
 import {
-    Container,
+  Container,
+ 
 } from './styles';
 
-export function MyCars(){
-  return(
+
+export function MyCars() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get(`/schedules_byuser?user_id=1`);
+        console.log(response.data);
+        setCars(response.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCars();
+  }, []);
+
+  return (
     <Container>
 
     </Container>
