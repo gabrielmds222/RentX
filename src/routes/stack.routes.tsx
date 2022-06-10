8fbb69034febd38f96df0596aeb38764cdfc7e1e
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-06-10 04:03:26 -0300

    feat: Cria tela de carros do usuário e adiciona navegação

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '../screens/Home';
+import { MyCars } from '../screens/MyCars';
 import { CarDetails } from '../screens/CarDetails';
 import { Scheduling } from '../screens/Scheduling';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
@@ -14,6 +15,7 @@ export function StackRoutes(){
     return (
         <Navigator headerMode="none">
             <Screen name="Home" component={Home} />
+            <Screen name="MyCars" component={MyCars} />
             <Screen name="CarDetails" component={CarDetails} />
             <Screen name="Scheduling" component={Scheduling} />
             <Screen name="SchedulingDetails" component={SchedulingDetails} />
