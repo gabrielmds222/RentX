06bcf89eeae3df23440303388ee8142e4fa23185
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-06-18 23:54:13 -0300

    feat: Flatlist para imagens dos carros

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FlatList } from 'react-native'
 
 import {
     Container,
@@ -23,9 +24,15 @@ export function ImageSlider({imagesUrl}: Props) {
         </ImageIndexes>
 
         <CarImageWrapper>
-            <CarImage
-                source={{uri: imagesUrl[0]}}
-                resizeMode="contain"
+            <FlatList 
+                data={imagesUrl}
+                keyExtractor={key => key}
+                renderItem={({ item }) => (
+                    <CarImage 
+                    source={{ uri: item }}
+                    resizeMode="contain" 
+                    />
+                )}
             />
         </CarImageWrapper>
 
