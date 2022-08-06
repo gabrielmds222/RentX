eefa65a432e75e2b0b763b7d7e3d56e1694a8ab8
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-08-06 18:50:11 -0300

    feat: Adiciona recursos de acessibilidade nos input do formulário

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { TextInputProps } from 'react-native';
+import React, { useState } from 'react';
 import { useTheme } from 'styled-components';
+import { TextInputProps } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 import {
@@ -9,26 +9,42 @@ import {
     InputText,
 } from './styles';
 
-interface InputProps extends TextInputProps {
-    iconName: React.ComponentProps<typeof Feather>['name']
+
+interface Props extends TextInputProps {
+    iconName: React.ComponentProps<typeof Feather>['name'],
+    value?: string;
 }
 
-export function Input({
-    iconName,
-    ...rest
-}: InputProps){
+export function Input({ iconName, value, ...rest} : Props) {
     const theme = useTheme();
-  return(
-    <Container >
-        <IconContainer>
-            <Feather 
-                name={iconName}
-                size={24}
-                color={theme.colors.text_detail}
-            />
-        </IconContainer>
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
 
-        <InputText {...rest}/>
-    </Container>
-  );
+    function handleInputFocus(){
+        setIsFocused(true);
+    }
+
+    function handleInputBlur(){
+        setIsFocused(false)
+        setIsFilled(!!value)
+    }
+
+    return (
+        <Container>
+            <IconContainer isFocused={isFocused}>
+            <Feather 
+                name={iconName} 
+                size={24} 
+                color={ (isFocused || isFilled) ? theme.colors.main : theme.colors.text_detail } 
+            />
+            </IconContainer>
+
+            <InputText 
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                isFocused={isFocused}
+                {...rest}
+            />
+        </Container>
+    );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   StatusBar, 
   KeyboardAvoidingView,
@@ -21,6 +21,9 @@ import {
 } from './styles';
 
 export function SignIn(){
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return(
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -47,11 +50,14 @@ export function SignIn(){
                 keyboardType='email-address'
                 autoCorrect={false}
                 autoCapitalize="none"
+                onChangeText={setEmail}
+                value={email}
               />
               <PasswordInput 
                 iconName="lock"
                 placeholder="Senha"
-                secureTextEntry
+                onChangeText={setPassword}
+                value={password}
               />
             </Form>
 
