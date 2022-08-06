636aec39c7be0781892513bd0414cb88fc1fb520
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-08-06 18:59:19 -0300

    fix: Refactor nos input e no componente de botão

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,43 +1,39 @@
 import React from 'react';
+import { useTheme } from 'styled-components'; 
 import { ActivityIndicator } from 'react-native';
-import { RectButtonProps } from 'react-native-gesture-handler';
-import { useTheme } from 'styled-components';
-
 import {
     Container,
     Title,
 } from './styles';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
-interface Props {
+interface Props extends RectButtonProps {
     title: string;
-    color?: string;
-    onPress: () => void;
-    enabled?: boolean;
+    color?: string; 
     loading?: boolean;
     light?: boolean;
 }
 
-export function Button({
-    title,
-    color,
-    onPress,
+export function Button({ 
+    title, 
+    color, 
+    onPress, 
     enabled = true,
     loading = false,
-    light = false,
-}: Props){
-  const theme = useTheme();
-  return(
-    <Container 
-    color={color ? color : theme.colors.main} 
-    onPress={onPress}
-    enabled={enabled}
-    style={{opacity: (enabled === false || loading === true) ? 0.5 : 1}}
-    >
-      {loading ? (
-        <ActivityIndicator color={theme.colors.shape} />
-      ) : (
-        <Title light={light}>{title}</Title>
-      )}
-    </Container>
-  );
+    light = false
+} : Props){
+    const theme = useTheme();
+
+    return (
+        <Container 
+        color={color ? color : theme.colors.main} 
+        onPress={onPress} 
+        enabled={enabled}
+        style={{ opacity: (enabled === false || loading === true) ? .5 : 1 }} 
+        >
+        { 
+         loading ? <ActivityIndicator color={theme.colors.shape} /> : <Title light={light}>{title}</Title>
+        }
+        </Container>
+    );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,27 +1,27 @@
-import styled from 'styled-components/native';
-import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { RectButton } from 'react-native-gesture-handler';
 import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
 
-interface ButtonProps extends RectButtonProps {
-  color?: string;
+interface ButtonProps{
+    color: string;
 }
 
 interface ButtonTextProps {
-  light: boolean;
+    light: boolean;
 }
 
 export const Container = styled(RectButton)<ButtonProps>`
-  width: 100%;
-  padding: 18px;
-  align-items: center;
-  justify-content: center;
-  background-color: ${({ color }) =>
-    color };
-  margin-bottom: 8px;
+    width: 100%;
+    padding: 19px;
+    align-items: center;
+    justify-content: center;
+
+    background-color: ${({ color }) => color };
+    margin-bottom: 8px;
 `;
 
 export const Title = styled.Text<ButtonTextProps>`
-  font-family: ${({ theme }) => theme.fonts.primary_500};
-  font-size: ${RFValue(15)}px;
-  color: ${({ theme, light }) => light ? theme.colors.header : theme.colors.shape};
+    font-family: ${({ theme }) => theme.fonts.primary_500};
+    font-size: ${RFValue(15)}px;
+    color: ${({ theme, light }) => light ? theme.colors.header : theme.colors.shape}
 `;
