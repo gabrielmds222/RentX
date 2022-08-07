dd33c271060280eea566fcab25e8b26a54511158
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-08-06 22:28:17 -0300

    feat: Renderiza componente BackButton em tela

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/core';
+
+import { BackButton } from '../../../components/BackButton';
 
 import {
     Container,
+    Header,
 } from './styles';
 
 export function SignUpFirstStep(){
+  const navigation = useNavigation();
+
+  function handleBack(){
+    navigation.goBack()
+}
+
   return(
     <Container>
-
+      <Header>
+        <BackButton onPress={handleBack}/>
+      </Header>
     </Container>
   );
 }
--- src/screens/SignUp/SignUpFirstStep/styles.ts
+++ src/screens/SignUp/SignUpFirstStep/styles.ts
@@ -3,3 +3,7 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
 
 `;
+
+export const Header = styled.View`
+
+`;
