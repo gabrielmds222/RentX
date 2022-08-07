c4a92d10cddb5b93ad477d88162dbddc84488bef
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-08-06 23:50:01 -0300

    feat: Validação do primeiro passo do cadastro

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -5,6 +5,7 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from 'react-native';
 
 import { BackButton } from '../../../components/BackButton';
@@ -22,15 +23,37 @@ import {
     FormTitle,
 } from './styles';
 
+import * as Yup from 'yup'
+
 export function SignUpFirstStep(){
   const navigation = useNavigation();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [driverLicense, setDriverLicense] = useState('');
 
   function handleBack(){
     navigation.goBack()
 }
 
-function handleNextStep(){
-  navigation.navigate('SignUpSecondStep');
+async function handleNextStep(){
+  try {
+      const schema = Yup.object().shape({
+          driverLicense: Yup.string().required('CNH é obrigatória').min(9, 'Minimo de nove caracteres'),
+          email: Yup.string().email('E-mail invalido').required('E-mail obrigatório'),
+          name: Yup.string().required('Nome obrigatório'),
+      });
+
+      const data = { name, email, driverLicense }
+      await schema.validate(data);
+
+      navigation.navigate('SignUpSecondStep', { user: data})
+  } catch (error) {
+      if(error instanceof Yup.ValidationError){
+          return Alert.alert('Opa', error.message)
+      }
+  }
+ 
 }
 
   return(
@@ -52,20 +75,26 @@ function handleNextStep(){
             <FormTitle>
               1. Dados
             </FormTitle>
-            <Input 
-            iconName="user" 
-            placeholder="Nome"
-            />
-            <Input 
-            iconName="mail" 
-            placeholder="E-mail"
-            keyboardType="email-address"
-            />
-            <Input 
-            iconName="credit-card" 
-            placeholder="CNH"
-            keyboardType="numeric"
-            />
+              <Input 
+                iconName='user'
+                placeholder='Nome'
+                onChangeText={setName}
+                value={name}
+              />
+              <Input 
+                iconName='mail'
+                placeholder='E-mail'
+                keyboardType='email-address'
+                onChangeText={setEmail}
+                value={email}
+              />
+              <Input 
+                iconName='credit-card'
+                placeholder='CNH'
+                keyboardType='numeric'
+                onChangeText={setDriverLicense}  
+                value={driverLicense}            
+              />
           </Form>
 
           <Button title="Próximo" onPress={handleNextStep}/>
