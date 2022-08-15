61dcf4a39de5c232e32a7300572eae128ccd166a
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-08-15 00:05:36 -0300

    fix: erro no cadastro do usuário

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/core';
-import { useTheme } from 'styled-components'
-
-import { 
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-  Keyboard,
-  Alert,
-} from 'react-native';
-
-import { api } from '../../../services/api';
-import { Complete } from '../../Complete';
-import { BackButton } from '../../../components/BackButton';
 import { Bullet } from '../../../components/Bullet';
+import { BackButton } from '../../../components/BackButton';
 import { PasswordInput } from '../../../components/PasswordInput';
 import { Button } from '../../../components/Button';
+import { KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native'
+import { useTheme } from 'styled-components'
+import { Complete } from '../../Complete'
 
 import {
     Container,
@@ -23,97 +15,104 @@ import {
     Title,
     SubTitle,
     Form,
-    FormTitle,
+    FormTitle
 } from './styles';
+import { api } from '../../../services/api'
 
 interface Params {
-  user: {
-      name: string;
-      email: string;
-      driverLicense: string;
-  }
+    user: {
+        name: string;
+        email: string;
+        driverLicense: string;
+    }
 }
 
 export function SignUpSecondStep(){
-  const navigation = useNavigation();
-  const route = useRoute();
-  const theme = useTheme();
+    const navigation = useNavigation();
+    const theme = useTheme();
+    const route = useRoute();
+    const [password, setPassword] = useState('')
+    const [passwordConfirm, setPasswordConfirm] = useState('');
 
-  const [password, setPassword] = useState('')
-  const [passwordConfirm, setPasswordConfirm] = useState('');
+    const { user } = route.params as Params;
 
-  const { user } = route.params as Params;
+    async function handleRegister(){
+        if(!password || !passwordConfirm){
+           return Alert.alert('Informe a senha e sua confirmação')
+        }
 
-  async function handleRegister(){
-    if(!password || !passwordConfirm){
-       return Alert.alert('Informe a senha e sua confirmação')
-    }
+        if(password != passwordConfirm){
+            return Alert.alert('Senhas nao sao iguais')
+        }
 
-    if(password != passwordConfirm){
-        return Alert.alert('Senhas nao sao iguais')
-    }
-
-    navigation.navigate('Complete', { 
-      nextScreenRoute: 'SignIn',
-      title: 'Conta criada',
-      message: `Agora é só fazer o login\n e aproveitar.`,
-    })
-
-    await api.post('/users', {
-        name: user.name,
-        email: user.email,
-        driver_license: user.driverLicense,
-        password
-    }).then(() => { 
-        navigation.navigate('Complete', {
-            nextScreenRoute: 'SignIn',
-            title: 'Conta Criada',
-            message: `Agora é só fazer login\ne aproveitar`
+        await api.post('/users', {
+            name: user.name,
+            email: user.email,
+            driver_license: user.driverLicense,
+            password
+        }).then(() => { 
+            navigation.navigate('Complete', {
+                nextScreenRoute: 'SignIn',
+                title: 'Conta Criada',
+                message: `Agora é só fazer login\ne aproveitar`
+            })
+        }).catch((error) => {
+            console.log(error)
+            Alert.alert('Opa', 'Não foi possível cadastrar')
         })
-    }).catch((error) => {
-        console.log(error)
-        Alert.alert('Opa', 'Não foi possível cadastrar')
-    })
-}
 
-  function handleBack(){
-    navigation.goBack()
-}
+       
+    }
+ 
 
-  return(
-    <KeyboardAvoidingView behavior="position" enabled>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>  
-        <Container>
-          <Header>
-            <BackButton onPress={handleBack}/>
-            <Steps>
-              <Bullet active={false}/>
-              <Bullet />
-            </Steps>
-          </Header>
+    function handleBack(){
+        navigation.goBack()
+    }
 
-          <Title>Crie sua {'\n'}conta</Title>
-          <SubTitle>Faça seu cadastro de{'\n'} forma rápida e fácil</SubTitle>
+    return (
+      <KeyboardAvoidingView behavior='position' enabled>
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <Container>
+                  <Header>
+                      <BackButton onPress={handleBack}/>
+                      <Steps>
+                      <Bullet />
+                      <Bullet active />
+                      </Steps>
+                  </Header>
 
-          <Form>
-            <FormTitle>2. Senha</FormTitle>
-              <PasswordInput 
-                iconName='lock'
-                placeholder='Senha'
-                onChangeText={setPassword}
-                value={password}
-              />
-              <PasswordInput 
-                iconName='lock'
-                placeholder='Repetir Senha'
-                onChangeText={setPasswordConfirm}
-                value={passwordConfirm}
-              />
-          </Form>
+                  <Title>
+                      Crie sua{'\n'}conta
+                  </Title>
 
-          <Button title="Cadastrar" color={theme.colors.success} onPress={handleRegister}/>
-        </Container>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
-  );
+                  <SubTitle>
+                      Faça seu cadastro de{'\n'}
+                      forma rápida e fácil
+                  </SubTitle>
+
+                  <Form>
+                      <FormTitle>2. SENHA</FormTitle>
+                      <PasswordInput 
+                          iconName='lock'
+                          placeholder='Senha'
+                          onChangeText={setPassword}
+                          value={password}
+                      />
+                      <PasswordInput 
+                          iconName='lock'
+                          placeholder='Repetir Senha'
+                          onChangeText={setPasswordConfirm}
+                          value={passwordConfirm}
+                      />
+                  </Form>
+
+                  <Button 
+                      title='Cadastrar'
+                      color={theme.colors.success}
+                      onPress={handleRegister}
+                  />
+              </Container>
+          </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,4 @@ const api = axios.create({
     baseURL: 'http://192.168.1.5:3333',
 });
 
-export default api;
+export { api };
